fe2b59fbab11cdb8ca13e0e94af2e16df933c49e
PhiMo17 <71430112+PhiMo17@users.noreply.github.com>
2021-02-16 12:47:37 +0100

    Add files via upload

---
+++ Cylinder/src/Circle.java
@@ -0,0 +1,25 @@
+
+public class Circle {
+	private double radius;
+
+	public Circle(double radius) {
+		if (radius <= 0) {
+			radius = 0;
+		}
+		else {
+		this.radius = radius;
+		}
+	}
+
+	/*public Circle(double radius2) {
+		// TODO Auto-generated constructor stub
+	}*/
+
+	public double getRadius() {
+		return radius;
+	}
+	
+	public double getArea() {
+		return (radius * radius * Math.PI);
+	}
+}
---
+++ Inheritance Challenge/src/Outlander.java
@@ -0,0 +1,11 @@
+
+public class Outlander extends Car{
+
+	private int roadService;
+	
+	public Outlander(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadService) {
+		super(name, size, wheels, doors, gears, isManual);
+		// TODO Auto-generated constructor stub
+		this.roadService = roadService;
+	}
+}
